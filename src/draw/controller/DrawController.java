3831441ec1d8d2a1ef24ea29607efafa29ed7787
Zac <zombiezac@att.net>
2019-05-03 13:16:24 -0600

    added some more constructors

--- src/draw/controller/DrawController.java
+++ src/draw/controller/DrawController.java
@@ -1,10 +1,15 @@
 package draw.controller;
 
+import draw.view.DrawFrame;
+
 public class DrawController
 {
+	private DrawFrame frame;
 	
-	
-	
+	public DrawController()
+	{
+		frame = new DrawFrame(this);
+	}
 	
 	
 	
--- src/draw/view/DrawFrame.java
+++ src/draw/view/DrawFrame.java
@@ -1,6 +1,29 @@
 package draw.view;
 
-public class DrawFrame
-{
+import javax.swing.*;
+import draw.controller.DrawController;
 
+public class DrawFrame extends JFrame
+{
+	private static final long serialVersionUID = 3L;
+	
+	private DrawController app;
+	private DrawPanel pane;
+	
+	public DrawFrame(DrawController app)
+	{
+		super();
+		
+		this.app = app;
+		pane = new DrawPanel();
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setSize(1000, 800);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
