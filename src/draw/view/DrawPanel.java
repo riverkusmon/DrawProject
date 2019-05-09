21abdbce3be11ed1102e3a8ccc5dabef2bcbe692
Andrew <andrewgardner@live.com>
2019-05-09 13:22:43 -0600

    added more methods in the drawpanel

--- src/draw/view/DrawPanel.java
+++ src/draw/view/DrawPanel.java
@@ -1,16 +1,63 @@
 package draw.view;
 
-import javax.swing.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import draw.controller.DrawController;
 
 public class DrawPanel extends JPanel
 {
 	private DrawController app;
+	private Color currentColor;
+	private BufferedImage currentCanvas;
+	private int previousX;
+	private int previousY;
 	
 	public DrawPanel(DrawController app)
 	{
 		super();
-		
 		this.app = app;
+		this.currentCanvas = new BufferedImage(700, 700, BufferedImage.TYPE_INT_ARGB);
+		setupPanel()
+		resetPoint();
 	}
+	
+	public void resetPoint()
+	{
+		previousX = Integer.MIN_VALUE;
+		previousY = Integer.MIN_VALUE;
+	}
+	
+	public void clearImage()
+	{
+		
+	}
+	
+	public void drawLine(int currentX, int currentY, int width)
+	{
+		
+	}
+	
+	public void drawDot(int currentX, int currentY, int width)
+	{
+		
+	}
+	
+	public void saveImage()
+	{
+		
+	}
+	
 }
